2303ff8f234dbabf3e3f3db8f9caf47f3e9b6c80
valdivinobiava496@gmail.com <valdivinobiava496@gmail.com>
2026-06-09 15:04:10 -0300

    sadfgasdg

---
+++ src/Components/Contato.tsx
@@ -0,0 +1,26 @@
+import "../styles/contato.css";
+
+export default function Formulario() {
+  return (
+    <div className="container formulario">
+      {/*useio dois nomes pra não ferrar o tamanho padrao do container */}
+      <div className="container-formulario">
+        <p>Envie sua dúvida</p>
+        <h3>Entre em contato</h3>
+        <p className="texto-usuario">
+          Entre em contato. Estamos dispostos a tirar qualquer dúvida, seja um
+          orçamento ou uma dúvida técnica de algum de nossos produtos. Estamos à
+          disposição para responder.😎
+        </p>
+      </div>
+      <div className="inputs-formularios">
+        <input type="email" name="" id="" placeholder="Seu melhor Email" />
+        <input
+          type="text"
+          placeholder="Motivo do contato. Ex: Gostei muito do plano X, poderia me dar um orçamento?"
+        />
+      </div>
+      <button className="btn-primary enviar">Enviar</button>
+    </div>
+  );
+}
